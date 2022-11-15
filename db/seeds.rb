95da82d908b1f8914511f46bdc41714757584b1f
Etienne Klepal <etienne.klepal@gmail.com>
2022-11-15 15:50:59 +0100

    everything works but isn't stylized

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do
+  title = Faker::TvShows::BrooklynNineNine.quote
+  content = Faker::Books::Dune.quote
+  Article.create(title:, content:)
+end
